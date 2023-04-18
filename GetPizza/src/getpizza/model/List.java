cffa95d3ee3d58bf0ea7dd50096af882c511dbb5
itslubin <lubinyee@gmail.com>
2023-04-18 14:36:59 +0200

    new ingridients JSON

--- GetPizza/src/getpizza/model/List.java
+++
@@ -1,9 +0,0 @@
-package getpizza.model;
-
-import com.modeliosoft.modelio.javadesigner.annotations.DataType;
-import com.modeliosoft.modelio.javadesigner.annotations.objid;
-
-@DataType
-@objid ("250919ee-1e55-45bf-94fc-37654a9d8cdb")
-public class List {
-}
